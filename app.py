5554da185f1dd270ce739b57b28392aa30a4b9a4
Achan112233 <131224571+Achan112233@users.noreply.github.com>
2025-01-19 21:23:24 -0500

    added api key

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from WeatherApp import requests
+import requests
 from dotenv import load_dotenv
+
 import os
 
 load_dotenv()
@@ -8,5 +9,8 @@ api_key = os.getenv("API_KEY")
 
 user_input = input("Enter City: ")
 
-weather_data = requests.get()
+weather_data = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{user_input}?key={api_key}")
+
+
+
 
